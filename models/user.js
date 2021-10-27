36d3a959006c8f02eb22c1753cba5d0e21e47245
Gordon Maloney <gordonmaloney@gmail.com>
2021-10-27 21:35:44 +0100

    google auth login/signup

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true },
+    //password: { type: String, required: true },
+    name: {type: String, required: true },
 });
 
 export default mongoose.model("User", userSchema);
