63065a62af9c29d49103cfc9fadf73b541499e93
Gordon Maloney <gordonmaloney@gmail.com>
2022-03-18 18:31:46 +0000

    heroku commit

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true },
     //password: { type: String, required: true },
     name: {type: String, required: true },
-    progress: {type: Number, default: 0},
+    progress: [{type: Number, default: 0}],
     words: [{
         level: Number,
         word: Number,
